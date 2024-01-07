6ff5fb6daa10e1322334be3e3fc1e54bf5bad31b
WellingtonDefassio <well.defassio@gmail.com>
2024-01-07 14:44:12 -0300

    formas de passar os parametros para uma função e valores padrão

--- src/main/kotlin/fundamentos/functions.kt
+++ src/main/kotlin/fundamentos/functions.kt
@@ -2,8 +2,14 @@ package io.wdefassio.fundamentos
 
 fun main() {
     println(retornaNome("Caldimiro"))
-}
+    println(dizOi("Matheus"))
+    println(dizOi("Matheus", 35))
+    println(dizOi(idade = 33, nome = "Matheus"))}
 
 fun retornaNome(nome: String): String {
     return "seu nome é $nome"
+}
+
+fun dizOi(nome: String, idade: Int = 20): String {
+    return "oi, $nome de $idade anos"
 }
